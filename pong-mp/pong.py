22ff037cf80d9c5a53faa33df96f8daa19ab3028
nicozk <nicozk@1cccdd93-c1d3-c715-c0c9-357b1930cadf>
2010-06-13 00:39:53 +0000

    nico

--- pong-mp/pong.py
+++ pong-mp/pong.py
@@ -2,11 +2,21 @@
 
 import pyglet
 from config_window import ConfigWindow
+import serverClientMsgParser
 
 class Application(object):
     def __init__(self):
         ConfigWindow(self)
-        self.http_client = None
-
+        self.client_socket = None
+        self.client_socket.on_received = self.client_socket_received
+        self.parser = serverClientMsgParser.ServerClientMsgParser()
+        self.parser.on_connected = self.parser_connected
+        
+    def client_socket_received(self, msg):
+        self.parser.parse(msg)
+        
+    def parser_connected(self):
+        pass
+    
 application = Application()
 pyglet.app.run()
